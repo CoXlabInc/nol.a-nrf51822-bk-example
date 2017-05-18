98550c258c2530e7b79be567cddcb7644d90822e
Jongsoo Jeong <jsjeong@coxlab.kr>
2017-05-18 23:14:23 +0900

    Added a beacon example.

--- LED-Blink/main.cpp
+++ LED-Blink/main.cpp
@@ -1,12 +1,19 @@
 #include <cox.h>
 
 Timer tBlink;
+Timer tOff;
 
-static void taskBlink(void *) {
-  System.ledToggle(0);
+static void taskOn(void *) {
+  System.ledOn(0);
+  tOff.startOneShot(10);
+}
+
+static void taskOff(void *) {
+  System.ledOff(0);
 }
 
 void setup() {
-  tBlink.onFired(taskBlink, NULL);
+  tOff.onFired(taskOff, NULL);
+  tBlink.onFired(taskOn, NULL);
   tBlink.startPeriodic(1000);
 }
